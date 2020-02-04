2a42aeca6d26d34da0abd263beaf19fa6e35023e
weiyang <weiyang.ones@gmail.com>
2020-02-04 13:13:18 +0800

    [background] Add 'inject_csvdiff'
    Signed-off-by: weiyang <weiyang.ones@gmail.com>

--- background.js
+++ background.js
@@ -9,6 +9,26 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
+function inject_csvdiff() {
+  chrome.tabs.insertCSS({
+    file: "css/daff.css"
+  });
+  chrome.tabs.executeScript({
+    file: "js/daff-1.3.40.js"
+  });
+  chrome.tabs.executeScript({
+    file: "js/jquery.slim.min-3.4.1.js"
+  });
+  chrome.tabs.executeScript({
+    file: "js/jquery.csv.min-1.0.8.js"
+  });
+  chrome.tabs.executeScript({
+    file: "csvdiff.js"
+  });
+}
+
+const MSG_LOSS_CONN = "Could not establish connection";
+
 // add render handler
 function csvdiff_render_handler(info, tab) {
   if (!tab) return;
@@ -17,7 +37,18 @@ function csvdiff_render_handler(info, tab) {
   chrome.tabs.sendMessage(tab.id, { action: "render_csv_files" }, function(
     response
   ) {
-    console.info(response);
+    if (!chrome.runtime.lastError) {
+      console.info(response);
+      return;
+    }
+
+    const msg = chrome.runtime.lastError.message;
+    if (msg.includes(MSG_LOSS_CONN)) {
+      inject_csvdiff();
+      console.info("Reinjected codes.");
+      return;
+    }
+    console.error("Got expected error: " + chrome.extension.lastError.message);
   });
 }
 
